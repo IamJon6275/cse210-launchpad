29d70b61a1bc797b22c837ebb491395686ade0e8
IamJon6275 <wel15010@byui.edu>
2024-09-27 21:27:35 -0600

    prep 3,4,5

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> numberList;
+        numberList = new();
+
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.WriteLine("Enter Input: ");
+            string inputString = Console.ReadLine();
+            input = int.Parse( inputString );
+            numberList.Add(input);
+        }
+
+        foreach (int number in numberList)
+        {
+            Console.WriteLine(number);
+        }
         Console.WriteLine("Hello Sandbox World!");
     }
 }
